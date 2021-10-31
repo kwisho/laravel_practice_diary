d131c70102670eac22ab4d11836321f914ae0ed1
kwisho <kwi.shota0912@gmail.com>
2021-10-31 15:53:22 +0900

    <初期設定 laravel-ui>

---
+++ backend/resources/views/submissions/index.blade.php
@@ -0,0 +1,45 @@
+<x-app>
+  <form action="" method="" class="form-horizontal">
+
+    <div class="form-group">
+      <label class="col-sm-3 control-label" for="name">会社名・事業所名</label>
+    <div class="col-sm-6">
+       <input type="text" class="form-control" name="">
+    </div>
+    </div>
+
+    <div class="form-group">
+      <label class="col-sm-3 control-label" for="email">メールアドレス</label>
+    <div class="col-sm-6">
+      <input type="text" class="form-control" name="">
+    </div>
+    </div>
+
+    <div class="form-group">
+      <label class="col-sm-3 control-label" for="ask1">対象のサービス<label>
+    <div class="col-sm-6">
+    <label class="form-control-static">
+　  　<input type="radio" name="" value="">Aサービス<br>
+　　  <input type="radio" name="" value="">Bサービス<br>
+　  　<input type="radio" name="" value="">Cサービス<br>
+　  　<input type="radio" name="" value="">その他<br>
+    </label>
+    </div>
+    </div>
+
+    <div class="form-group">
+      <label class="col-sm-3 control-label" for="ask1">お問い合わせ内容</label>
+    <div class="col-sm-6">
+    <textarea rows="7" name="" class="form-control"></textarea>
+    </div>
+    </div>
+
+    <div class="form-group">
+      <label class="col-sm-3"></label>
+    <div class="col-sm-9">
+       <button class="btn btn-default">リセット</button>
+       <button class="btn btn-default">送信する</button>
+    </div>
+    </div>
+</form>
+</x-app>
